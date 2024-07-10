e4ef224dd8fe4879c823ec53c632d934a096ff22
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2024-07-10 01:59:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,9 @@
-import turtle
+from numpy import array
+from random import *
 
-def draw_heart():
-    window = turtle.Screen()
-    window.bgcolor("white")
+t = array([int]*15)
 
-    heart = turtle.Turtle()
-    heart.color("red")
-    heart.speed(2)
-    heart.pensize(2)
+for i in range(15):
+    t[i] = randint(0,1500)
 
-    heart.begin_fill()
-
-    # Start drawing the heart
-    heart.left(140)
-    heart.forward(113)
-    for _ in range(200):
-        heart.right(1)
-        heart.forward(1)
-    heart.left(120)
-    for _ in range(200):
-        heart.right(1)
-        heart.forward(1)
-    heart.forward(113)
-
-    heart.end_fill()
-
-    heart.hideturtle()
-    window.exitonclick()
-
-draw_heart()
+st.table(t)
